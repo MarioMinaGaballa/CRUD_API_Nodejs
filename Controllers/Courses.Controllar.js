4bbd13d1372369e7e86b2e6a8832459c287d6a1d
MarioMinaGaballa <mariomena199@gmail.com>
2024-09-13 20:00:05 +0200

    Finish Video_7

--- Controllers/Courses.Controllar.js
+++ Controllers/Courses.Controllar.js
@@ -2,10 +2,11 @@
 const { validationResult } = require("express-validator");
 const Course = require("../models/course.model");
 const httpStatusText = require("../utils/http.status.text"); 
+const asyncMiddleware = require("../middleware/async.middleware");
+const AppError =require("../utils/appError")
 
 
-
-const getAllCourses = async (req, res) => {
+const getAllCourses =asyncMiddleware( async (req, res) => {
   const query = req.query
 const limit = query.limit|| 10;//2
 const page =query.page || 1;//3
@@ -15,63 +16,54 @@ const skip = (page-1)*limit
   // get all courses from DB using Course Model
   const courses = await Course.find({},{"__v":false}).limit(limit).skip(skip);
   res.json({ status: httpStatusText.SUCCESS, data: { courses } });
-};
+})
 
-const getSingleCourses = async (req, res) => {
-  try {
-    const course = await Course.findById(req.params.courseId);
-    if (!course) {
-      return res
-        .status(404)
-        .json({
-          status: httpStatusText.FAIL,
-          data: { course: "Course Not Found" }
-          
-        });
+const getSingleCourses = asyncMiddleware(
+  async (req, res,next) => {
+
+      const course = await Course.findById(req.params.courseId, { "__v": false });
+      if (!course) {
+      const error= AppError.create("not found course",404,httpStatusText.FAIL)
+        return next(error);
+      }
+      
+      res.json({ status: httpStatusText.SUCCESS, data: { course } });
     }
-    // course.find({},{"__v":false})
-    
-    res.json({ status: httpStatusText.SUCCESS, data: { course } });
-  } catch (err) {
-    return res
-      .status(400)
-      .json({
-        status: httpStatusText.ERROR,
-        data: null,
-        message: err.message,
-        code: 400,
-      });
-  }
-};
+)
 
-const addCourse = async (req, res) => {
+
+const addCourse =asyncMiddleware( async (req, res) => {
   const err = validationResult(req);
   if (!err.isEmpty()) {
-    return res.status(400).json({status :httpStatusText.FAIL,data:err.array()});
+   const error =AppError.create(err.array(),400,httpStatusText.FAIL)
+    return next(error)
   }
 
   const newCourse = new Course(req.body);
   await newCourse.save();
-  res.status(201).json({status :httpStatusText.SUCCESS,data:{course:newCourse}});
-};
+  
+  // Remove __v field from the response
+  const courseResponse = newCourse.toObject();
+  delete courseResponse.__v;
 
-const updateCourse = async (req, res) => {
-  try{
+  res.status(201).json({status: httpStatusText.SUCCESS, data: {course: courseResponse}});
+})
+
+
+const updateCourse =asyncMiddleware(async (req, res) => {
     const course = await Course.updateOne(
       { _id: req.params.courseId },
       { $set: { ...req.body } }
     );
     return res.status(200).json({status :httpStatusText.SUCCESS,data:{course:course}});
-  }catch(err){
-     return res.status(400).json({status:httpStatusText.ERROR,message:err.message})
-  }
 
-};
+})
 
-const deleteCourse = async (req, res) => {
+
+const deleteCourse =asyncMiddleware( async (req, res) => {
   await Course.deleteOne({ _id: req.params.courseId });
   return res.status(200).json({ status: httpStatusText.SUCCESS, data: null });
-};
+});
 
 module.exports = {
   getAllCourses,
--- index.js
+++ index.js
@@ -1,21 +1,33 @@
+require("dotenv").config()
 const express = require("express");
+const cors=require("cors")
 const app = express();
-
+const httpStatusText=require('./utils/http.status.text')
 
 const mongoose =require("mongoose")
-const url =
-  "mongodb+srv://mariomena199:nodejs@learn-mongodb.yfjlw.mongodb.net/App?retryWrites=true&w=majority&appName=Learn-mongoDb";
+const url =process.env.MONGO_URL
+const port = process.env.PORT
+  
 mongoose.connect(url).then(()=>{
   console.log("Connected Successfully To DataBase");
 })
 
-
-const port = 4000;
 const courseRouter=require('./routes/courses.route')
+
+app.use(cors())
 app.use(express.json());
 app.use('/api/courses',courseRouter)
 
 
+//global middleware for not found router
+app.use("*",(req,res,next)=>{
+  return res.status(404).json({status:httpStatusText.ERROR,message:"this resourse not avaliable"})
+})
+//global error handler 
+app.use((error,req,res,next)=>{
+  res.status(error.statusCode || 500).json({status:httpStatusText.ERROR|| httpStatusText.ERROR,message:error.message,code:error.statusCode || 500,data:null })
+})
+
 app.listen(port, () => {
-  console.log("listening in port 4000");
+  console.log(`listening in port ${port}`);
 });
