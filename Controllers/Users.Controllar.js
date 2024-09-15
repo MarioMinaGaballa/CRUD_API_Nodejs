8d9bc7fee7d0c40635a6bfe40478711570cf321b
MarioMinaGaballa <mariomena199@gmail.com>
2024-09-15 23:58:15 +0200

    JWT

--- Controllers/Users.Controllar.js
+++ Controllers/Users.Controllar.js
@@ -2,16 +2,16 @@ const asyncMiddleware = require("../middleware/async.middleware");
 const httpStatusText = require("../utils/http.status.text"); 
 const User =require("../models/user.model");
 const appError =require("../utils/appError");
+const bycrpt =require("bcryptjs");
+const jwt =require("jsonwebtoken");
 
 const getAllUsers =asyncMiddleware( async (req, res) => {
   const query = req.query
 const limit = query.limit|| 10;//2
 const page =query.page || 1;//3
 const skip = (page-1)*limit
-
-
   // get all courses from DB using Course Model
-  const  users = await  User.find({},{"__v":false}).limit(limit).skip(skip);
+  const  users = await  User.find({},{"__v":false,"password":false}).limit(limit).skip(skip);
   res.json({ status: httpStatusText.s, data: { users } });
 })
 
@@ -19,17 +19,29 @@ const skip = (page-1)*limit
 
 const register =asyncMiddleware(async (req, res,next) => {
   const { firstName, lastName, email, password } = req.body;
+
   const olduser= await User.findOne({email})
   if(olduser){
    const error=appError.create("user already exist",400,httpStatusText.FAIL)
    next(error)  
   }
+
+  //password hashing (Security)
+  const hashedPassword = await bycrpt.hash(password, 12);
+
+
   const newuser = await User({
     firstName,
     lastName,
     email,
-    password,
+    password:hashedPassword,
   });
+  //genrate token
+const token = await  jwt.sign({email:newuser.email},process.env.JWT_SECRET_KEY)
+console.log(token);
+return;
+
+
   await  newuser.save();
    return res.status(201).json({ status: httpStatusText.SUCCESS, data: { newuser } });
 });
@@ -38,7 +50,28 @@ const register =asyncMiddleware(async (req, res,next) => {
 
 
 const Login =asyncMiddleware(async (req, res,next) => {
+       
+  const { email, password } = req.body;
   
+ if(!email||!password){
+  const error=appError.create("email and password are required",400,httpStatusText.FAIL)
+  return  next(error)
+ }
+ const user = await User.findOne({ email }); 
+
+if(!user){
+  const error=appError.create("User Not Found",400,httpStatusText.FAIL)
+  return  next(error)
+}
+
+
+ const matcedPassword = await bycrpt.compare(password, user.password)
+ if(user&&matcedPassword){
+  return res.status(200).json({ status: httpStatusText.SUCCESS, data: { user:"loged in successfully" } });
+ }else{
+  const error=appError.create("Email Or Password is Wrong Please Try Again",400,httpStatusText.FAIL)
+  return  next(error)
+ }
 })
 
 
